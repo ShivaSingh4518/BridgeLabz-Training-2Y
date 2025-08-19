5c55f985f5556f00c82fd0d64877f0db96826328
Shiva Singh <shiva.singh_cs24@gla.ac.in>
2025-08-19 12:38:02 +0530

    Delete que4.java

--- que4.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class que4 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int radius = sc.nextInt();
-        System.out.println("Area of Circle: " +  3.14 * radius * radius);
-        sc.close();
-    }
-}
