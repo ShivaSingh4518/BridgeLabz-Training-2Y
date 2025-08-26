5d9bbc14a0eb0b3c61c60b704367ee6932e9f92a
Shiva Singh <shiva.singh_cs24@gla.ac.in>
2025-08-26 15:01:34 +0530

    Update PrimeNumberCheck.java

--- Java Control Flow/level3/PrimeNumberCheck.java
+++ Java Control Flow/level3/PrimeNumberCheck.java
@@ -4,26 +4,21 @@ public class PrimeNumberCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Taking user input
         System.out.print("Enter a number: ");
         int number = sc.nextInt();
-
-        // A number less than or equal to 1 is NOT a prime number
         boolean isPrime = true;
 
         if (number <= 1) {
             isPrime = false;
         } else {
-            // Check divisibility from 2 to number-1
             for (int i = 2; i < number; i++) {
                 if (number % i == 0) {
                     isPrime = false;
-                    break;  // No need to check further if divisible
+                    break;  
                 }
             }
         }
 
-        // Display result
         if (isPrime) {
             System.out.println(number + " is a Prime Number.");
         } else {
@@ -32,4 +27,5 @@ public class PrimeNumberCheck {
 
         sc.close();
     }
+
 }
